23813b1f1ebcec87b178a2d812ba252c7a39839d
o1123516766542617 <o1123516766542617@outlook.com>
2019-12-10 17:27:33 -0500

    add actions to reducer

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,11 +1,13 @@
 /// tools ///
+import { flag } from 'tools/hi';
 import { boolean } from 'tools/iffy';
+import immutably from 'tools/immutably';
 
 /**************************************/
 
 export const init = {
-  'todosList' : () => ([]),
-  'todoItem' : (text, isComplete) => ({
+  'list' : () => ([]),
+  'item' : (text, isComplete) => ({
     'id' : (Date.now ()),
     'text' : text,
     'isComplete' : (boolean (isComplete) ? isComplete : false),
@@ -13,10 +15,64 @@ export const init = {
 };
 
 export const actions = [
+  // add/delete item
+  'ADD_ITEM',
+  'DELETE_ITEM',
+  // mark/toggle specific item
+  'MARK_ITEM',
+  'UNMARK_ITEM',
+  'TOGGLE_ITEM',
+  // mark/toggle all items
+  'MARK_ALL_ITEMS',
+  'UNMARK_ALL_ITEMS',
+  'TOGGLE_ALL_ITEMS',
 ];
 
-export const reducer = (state, action) => {
-  switch (action.type) {
-    default : return (state);
+export const reducer = (state, { type, data }) => {
+  switch (type) {
+    // add/delete item
+    case 'ADD_ITEM' :
+      return ([
+        ...state,
+        init.item (data.text)
+      ]);
+    case 'DELETE_ITEM' :
+      return (
+        state.filter ((item) => (item.id !== data.item.id))
+      );
+    // mark/toggle specific item
+    case 'MARK_ITEM' :
+      const i = state.indexOf (data.item.id);
+      return (
+        immutably.set (state, [i, 'isComplete'], true)
+      );
+    case 'UNMARK_ITEM' :
+      const i = state.indexOf (data.item.id);
+      return (
+        immutably.set (state, [i, 'isComplete'], false)
+      );
+    case 'TOGGLE_ITEM' :
+      const i = state.indexOf (data.item.id);
+      return (
+        immutably.toggle (state, [i, 'isComplete'])
+      );
+    // mark/toggle all items
+    case 'MARK_ALL_ITEMS' :
+      return (state.map ((item) => (
+        immutably.set (item, ['isComplete'], true)
+      )));
+    case 'UNMARK_ALL_ITEMS' :
+      return (state.map ((item) => (
+        immutably.set (item, ['isComplete'], false)
+      )));
+    case 'TOGGLE_ALL_ITEMS' :
+      return (state.map ((item) => (
+        immutably.toggle (item, ['isComplete'])
+      )));
+    // else
+    default :
+      flag ('warn', 'action not defined');
+      flag ('warn', type);
+      return (state);
   };
 };
