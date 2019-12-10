df3e304aea9befb1de0661abd6c8884ec8e86a36
o1123516766542617 <o1123516766542617@outlook.com>
2019-12-10 16:09:19 -0500

    init project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+/// external modules ///
+import React from 'react';
+
+/// styles ///
+import './styles/App.css';
+
+/***************************************
+  COMPONENT
+***************************************/
+const App = () => {
+  return (
+    <div className='App'>
+    </div>
+  );
+}
+
+/**************************************/
+
+export default App;
