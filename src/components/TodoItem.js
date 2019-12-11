fb27fd222b5b076ce9699b8d52e17b5a030c50e6
o1123516766542617 <o1123516766542617@outlook.com>
2019-12-11 18:04:55 -0500

    add strikethrough to complete items

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,24 +1,31 @@
 /// external modules ///
 import React from 'react';
+import styled from 'styled-components';
 
 /***************************************
   MAIN
 ***************************************/
+const ListItem = styled.li `
+  &.is-complete {
+    text-decoration: line-through;
+  }
+`
+
 const TodoItem = ({ item, effect, ...rest }) => {
   console.log ('components/TodoList ~ item :', item);
 
   try {
     return (
-      <li
-      className='TodoItem'
+      <ListItem
+      className={`TodoItem ${item.isComplete ? 'is-complete' : ''}`.trim ()}
       onClick={() => effect.TOGGLE_ITEM (item)}
       >
         {item.text}
-      </li>
+      </ListItem>
     );
   }
   catch (error) {
-    return (<li>OOPS!</li>)
+    return (<ListItem>OOPS!</ListItem>)
   }
 };
 
