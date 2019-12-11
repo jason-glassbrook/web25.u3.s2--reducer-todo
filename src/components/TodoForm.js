c8c77afe37e06343a6747b66b4912a409c4aee4b
o1123516766542617 <o1123516766542617@outlook.com>
2019-12-10 20:12:15 -0500

    create TodoForm

---
+++ src/components/TodoForm.js
@@ -0,0 +1,50 @@
+/// external modules ///
+import React from 'react';
+
+/// tools ///
+import immutably from 'tools/immutably';
+
+/***************************************
+  MAIN
+***************************************/
+const TodoForm = ({ dispatcher, ...rest }) => {
+  // form state
+  const [state, setState] = React.useState ({ text : '' });
+
+  // handle change
+  const handleChange = (e) => setState ((state) => (
+    immutably.set (state, [e.target.name], e.target.value)
+  ));
+
+  return (
+    <form
+    className='TodoForm'
+    onSubmit={(e) => e.preventDefault ()}>
+      <div className='add_item'>
+        <input
+        type='text'
+        name='text'
+        placeholder='to-do'
+        value={state.text}
+        onChange={handleChange}
+        />
+        <button
+        type='submit'
+        name='add_item'
+        onClick={dispatcher.item.add (state)}>
+          add item
+        </button>
+      </div>
+      <button
+      type='button'
+      name='unmark_all_items'
+      onClick={dispatcher.list.unmark}>
+        clear completion on all items
+      </button>
+    </form>
+  );
+};
+
+/**************************************/
+
+export default TodoForm;
