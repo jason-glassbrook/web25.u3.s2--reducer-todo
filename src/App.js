c63e69cf4fa250a4ef93f08974302ee1acde4d07
o1123516766542617 <o1123516766542617@outlook.com>
2019-12-10 20:12:40 -0500

    add components and use reducer, dispatcher

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 /// external modules ///
 import React from 'react';
 
+/// reducers ///
+import todo from 'reducers/todo';
+
+/// components ///
+import TodoList from 'components/TodoList';
+import TodoForm from 'components/TodoForm';
+
 /// styles ///
 import './styles/App.css';
 
@@ -8,8 +15,19 @@ import './styles/App.css';
   COMPONENT
 ***************************************/
 const App = () => {
+  // reducer
+  const [state, dispatch] = React.useReducer (todo.reducer);
+  const dispatcher = todo.dispatcher (dispatch);
+
   return (
     <div className='App'>
+      <TodoForm
+      dispatcher={dispatcher}
+      />
+      <TodoList
+      list={state.list}
+      dispatcher={dispatcher}
+      />
     </div>
   );
 }
